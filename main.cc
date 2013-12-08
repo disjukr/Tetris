6efb22d1358bbfe876a9cfec3fea33cbf81eebd7
JongChan Choi <disjukr@naver.com>
2013-12-08 20:28:46 +0900

    remove sigaction code

--- main.cc
+++ main.cc
@@ -16,12 +16,7 @@ void signalHandler(int sig) {
 int main() {
     // handle signal
     signal(SIGINT, signalHandler);
-    struct sigaction signalActionHandler;
-    signalActionHandler.sa_handler = signalHandler;
-    sigemptyset(&signalActionHandler.sa_mask);
-    signalActionHandler.sa_flags = 0;
-    sigaction(SIGINT, &signalActionHandler, NULL);
-    sigaction(SIGTERM, &signalActionHandler, NULL);
+    signal(SIGTERM, signalHandler);
     // game logic
     game = new Tetris();
     game -> SetFps(24);
