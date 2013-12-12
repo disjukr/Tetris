76ac21efadbebfb913ecff709eebdb0a6c6f6302
JongChan Choi <disjukr@naver.com>
2013-12-12 11:46:19 +0900

    TMino

--- Tetromino.hh
+++ Tetromino.hh
@@ -55,3 +55,45 @@ public:
         return (x == 1 || x == 2) && (y == 1 || y == 2);
     }
 };
+
+class TMino: public Tetromino {
+    bool vertical;
+    bool tail;
+public:
+    TMino() {
+        vertical = true;
+        tail = true;
+        color = MAGENTA;
+        x = 0;
+        y = 0;
+    }
+    ~TMino() {}
+    virtual void RotateCW() {
+        if (vertical)
+            tail = !tail;
+        vertical = !vertical;
+    }
+    virtual void RotateCCW() {
+        if (!vertical)
+            tail = !tail;
+        vertical = !vertical;
+    }
+    virtual bool CheckBlock(int x, int y) {
+        if (!Tetromino::CheckBlock(x, y))
+            return false;
+        if (y == 0 || x == 3 ||
+            ((y == 1 || y == 3) && (x == 0 || x == 2)))
+            return false;
+        if (vertical) {
+            if (x == 1 &&
+                ((tail && y == 1) || (!tail && y == 3)))
+                return false;
+        }
+        else {
+            if (y == 2 &&
+                ((tail && x == 0) || (!tail && x == 2)))
+                return false;
+        }
+        return true;
+    }
+};
