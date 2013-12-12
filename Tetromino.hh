bcb749bee4ae041832633b1b448ecedaa0274f85
JongChan Choi <disjukr@naver.com>
2013-12-12 19:09:36 +0900

    J, LMino

--- Tetromino.hh
+++ Tetromino.hh
@@ -153,3 +153,93 @@ public:
             return y == 3 && x > 1;
     }
 };
+
+class JMino: public Tetromino {
+    bool vertical;
+    bool tail;
+public:
+    JMino() {
+        vertical = true;
+        tail = true;
+        color = BLUE;
+        x = 0;
+        y = 0;
+    }
+    ~JMino() {}
+    virtual void RotateCW() {
+        if (vertical)
+            tail = !tail;
+        vertical = !vertical;
+    }
+    virtual void RotateCCW() {
+        if (!vertical)
+            tail = !tail;
+        vertical = !vertical;
+    }
+    virtual bool CheckBlock(int x, int y) {
+        if (!Tetromino::CheckBlock(x, y))
+            return false;
+        if (vertical) {
+            if (x == 1 && y > 0)
+                return true;
+            if (tail && x == 0 && y == 3)
+                return true;
+            if (!tail && x == 2 && y == 1)
+                return true;
+        }
+        else {
+            if (y == 2 && x < 3 )
+                return true;
+            if (!tail && x == 0 && y == 1)
+                return true;
+            if (tail && x == 2 && y == 3)
+                return true;
+        }
+        return false;
+    }
+};
+
+class LMino: public Tetromino {
+    bool vertical;
+    bool tail;
+public:
+    LMino() {
+        vertical = true;
+        tail = true;
+        color = YELLOW;
+        x = 0;
+        y = 0;
+    }
+    ~LMino() {}
+    virtual void RotateCW() {
+        if (vertical)
+            tail = !tail;
+        vertical = !vertical;
+    }
+    virtual void RotateCCW() {
+        if (!vertical)
+            tail = !tail;
+        vertical = !vertical;
+    }
+    virtual bool CheckBlock(int x, int y) {
+        if (!Tetromino::CheckBlock(x, y))
+            return false;
+        if (vertical) {
+            if (x == 1 && y > 0)
+                return true;
+            if (tail && x == 2 && y == 3)
+                return true;
+            if (!tail && x == 0 && y == 1)
+                return true;
+        }
+        else {
+            if (y == 2 && x < 3 )
+                return true;
+            if (!tail && x == 0 && y == 3)
+                return true;
+            if (tail && x == 2 && y == 1)
+                return true;
+        }
+        return false;
+    }
+};
